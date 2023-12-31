4843f692d7b55de54e99c87418957dda54979956
tamani phiri <tamanigabriel0@gmail.com>
2024-01-01 00:15:17 +0200

    c

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginUserDto } from './dto/loginDto';
 
 @Injectable()
 export class AuthService {
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+    const user = await this.prismaService.user.findFirst({
+      where: { email },
+    });
+    if (user) throw new ConflictException('user already exists');
+  }
   constructor(private readonly prismaService: PrismaService) {}
   register(createUserDto: Prisma.UserCreateInput) {
     const { email, password, username } = createUserDto;
