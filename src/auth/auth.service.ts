a9bdd06c060460fffa3e1b2a278092929c126d5b
tamani phiri <tamanigabriel0@gmail.com>
2023-12-31 23:39:54 +0200

    c

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
   register(createUserDto: Prisma.UserCreateInput) {
-    throw new Error('Method not implemented.');
+    const { email, password, username } = createUserDto;
+    return this.prismaService.user.create({
+      data: {
+        username,
+        email,
+        password,
+      },
+    });
   }
 }
