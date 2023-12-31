f075a3d84b592bb6aed70318b14692f261d6915d
tamani phiri <tamanigabriel0@gmail.com>
2023-12-31 23:22:02 +0200

    c

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('register')
+  register(@Body() createUserDto: Prisma.UserCreateInput) {
+    return this.authService.register(createUserDto);
+  }
+}
