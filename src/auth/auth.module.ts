d2581cffa7c4f78fecfbc9620c94175929bd5dc0
tamani phiri <tamanigabriel0@gmail.com>
2023-12-31 23:36:18 +0200

    c

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  imports: [PrismaService],
   controllers: [AuthController],
   providers: [AuthService],
 })
