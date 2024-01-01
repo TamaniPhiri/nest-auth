76e873a7001e68386f5492cd1387b925321ea29f
tamani phiri <tamanigabriel0@gmail.com>
2024-01-01 10:51:06 +0200

    c

--- src/auth/strategy.service.ts
+++ src/auth/strategy.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {}
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+}
