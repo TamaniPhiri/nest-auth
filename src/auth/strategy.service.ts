3f3d1a0966f8083414907e214ae2318d5421fd59
tamani phiri <tamanigabriel0@gmail.com>
2024-01-01 11:02:57 +0200

    c

--- src/auth/strategy.service.ts
+++ src/auth/strategy.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+interface Payload {
+  sub: number;
+  email: string;
+}
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET,
-      ignoreExpiration: true,
+      ignoreExpiration: false,
     });
+  }
+
+  async validate(payload: Payload) {
+
   }
 }
