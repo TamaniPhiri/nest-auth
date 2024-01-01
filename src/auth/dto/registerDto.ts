26b41e8fc729247cd9e6dac3184ec38b4b9dad02
tamani phiri <tamanigabriel0@gmail.com>
2024-01-02 00:08:22 +0200

    c

--- src/auth/dto/registerDto.ts
+++ src/auth/dto/registerDto.ts
@@ -3,11 +3,9 @@ import { IsEmail, IsNotEmpty } from 'class-validator';
 export class RegisterDto {
   @IsNotEmpty()
   readonly username: string;
-
   @IsEmail()
   @IsNotEmpty()
   readonly email: string;
-
   @IsNotEmpty()
   readonly password: string;
 }
