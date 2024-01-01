d005ba01cd991737070604b086d8a5e47c2703ab
tamani phiri <tamanigabriel0@gmail.com>
2024-01-01 10:52:38 +0200

    c

--- src/auth/strategy.service.ts
+++ src/auth/strategy.service.ts
@@ -8,6 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET,
+      ignoreExpiration: true,
     });
   }
 }
