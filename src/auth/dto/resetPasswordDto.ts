31b0fdb293dae9aec69af2c71dedd31f21a80c9e
tamani phiri <tamanigabriel0@gmail.com>
2024-01-01 02:17:13 +0200

    c

--- src/auth/dto/resetPasswordDto.ts
+++ src/auth/dto/resetPasswordDto.ts
@@ -1,8 +1,7 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class ResetPasswordDto {
+  @IsNotEmpty()
   @IsEmail()
   readonly email: string;
-  @IsNotEmpty()
-  readonly password: string;
 }
